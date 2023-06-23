ffd09104f0fe44e744a2be158a947389d43576d0
adityasept02 <135803953+adityasept02@users.noreply.github.com>
2023-06-22 23:58:34 -0700

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -3,19 +3,6 @@ using namespace std;
 #define ll int long long
 int main()
 {
-    ll t;
-    cin >> t;
-    while (t--)
-    {
-        ll n;
-        cin >> n;
-        ll ans = 0;
-        while (n)
-        {
-            ans += n;
-            n = n / 2;
-        }
-        cout << ans << endl;
-    }
+    cout<<"hi i am aditya"<<endl;
     return 0;
 }
